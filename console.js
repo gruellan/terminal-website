f20335768e714acd028ff995ba73de0b20f5cce4
George Ruellan <ruellan.george@gmail.com>
2020-12-15 17:12:57 +0000

    Add ls functionality

--- console.js
+++ console.js
@@ -1,5 +1,6 @@
 
 $(function () {
+    // Type out the welcome lines
     $("#firstline").typed({
         strings: ["cat welcome.txt"],
         typeSpeed: 10,
@@ -16,4 +17,33 @@ $(function () {
             })
         }
     });
+
+    // Handle command when enter key pressed
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            var command = $("#input").val();
+            handleCommand(command);
+        }
+    });
+
+    function handleCommand(command) {
+        if (command == "ls") {
+            var line = "welcome.txt about.txt yeet.txt";
+            $("#container").append("<p>" + line + "</p>")
+            
+            // Remove id from old input and add it to new input line
+            $("#input").removeAttr('id');
+            $("#container").append('<p><span class="prompt" id="prompt">george@ruellan ~ $ </span><input id="input" autofocus></input></p>');
+            
+            // Refocus on the new input
+            $("#input").focus();
+        } else {
+            alert("nope chuck testa")
+        }
+    }
+
+    // Always have the terminal focused
+    $("body").click(function () {
+        $("#input").focus();
+    })
 });
